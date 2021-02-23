c325f4dff020acb01b5307d264264ef698ddb119
Jay P <Jayp3@jayp.com>
2021-02-23 16:29:39 -0600

    20200223 Day one before Homework.

---
+++ src/components/Channel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Channel() {
+    return (
+        <div>
+            <h1>Channel0  </h1>
+        </div>
+    )
+}
+
+export default Channel
---
+++ src/components/Chat.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Chat() {
+    return (
+        <div>
+            <h4>Chat Page  (Component)</h4>
+        </div>
+    )
+}
+
+export default Chat
